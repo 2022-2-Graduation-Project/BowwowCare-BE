9cad652ef7632c8c3899f892635afe6b1dad8b49
Juuuic <sn990908a@gmail.com>
2022-12-02 18:15:41 +0900

    feat: CORS 설정

--- src/main/java/com/bowwowcare/sm/controller/SurveyController.java
+++ src/main/java/com/bowwowcare/sm/controller/SurveyController.java
@@ -12,15 +12,13 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @Tag(name = "survey", description = "문진 API")
 @RequestMapping("/api")
 @RequiredArgsConstructor
 @RestController
+@CrossOrigin
 public class SurveyController {
 
     private final SurveyService surveyService;
--- src/main/java/com/bowwowcare/sm/controller/TestController.java
+++ src/main/java/com/bowwowcare/sm/controller/TestController.java
@@ -10,6 +10,7 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,6 +19,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api")
 @RequiredArgsConstructor
 @RestController
+@CrossOrigin
 public class TestController {
 
     @Operation(summary = "테스트 get api", description = "^~^")
