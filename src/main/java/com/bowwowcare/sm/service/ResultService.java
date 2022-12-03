2478488f94994f0012b63073e23335b2e3de3a30
Juuuic <sn990908a@gmail.com>
2022-12-03 17:44:41 +0900

    feat: analyzeResult 저장

---
+++ src/main/java/com/bowwowcare/sm/service/ResultService.java
@@ -0,0 +1,27 @@
+package com.bowwowcare.sm.service;
+
+import com.bowwowcare.sm.domain.result.Result;
+import com.bowwowcare.sm.domain.result.ResultRepository;
+import com.bowwowcare.sm.dto.result.ResultResponseDto;
+import com.bowwowcare.sm.dto.result.ResultSaveRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class ResultService {
+
+    private final ResultRepository resultRepository;
+
+    @Transactional
+    public Boolean saveAnalyzeResult(ResultSaveRequestDto resultSaveRequestDto) {
+        Result new_result = resultSaveRequestDto.toResultEntity();
+        resultRepository.save(new_result);
+        return true;
+    }
+
+
+
+
+}
