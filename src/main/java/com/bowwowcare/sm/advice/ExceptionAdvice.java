50f478d2fbc5b3d612d83cac33c90faf9e3ecd00
Juuuic <sn990908a@gmail.com>
2023-01-30 21:34:17 +0900

    feat: exception advice 추가

---
+++ src/main/java/com/bowwowcare/sm/advice/ExceptionAdvice.java
@@ -0,0 +1,11 @@
+package com.bowwowcare.sm.advice;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+@RequiredArgsConstructor
+public class ExceptionAdvice {
+
+
+}
