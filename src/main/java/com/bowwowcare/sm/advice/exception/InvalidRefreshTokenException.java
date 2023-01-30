e6640e1897d351a9888c79d24def2ccf0daad74b
Juuuic <sn990908a@gmail.com>
2023-01-30 23:08:11 +0900

    feat: InvalidRefreshTokenException 생성

---
+++ src/main/java/com/bowwowcare/sm/advice/exception/InvalidRefreshTokenException.java
@@ -0,0 +1,15 @@
+package com.bowwowcare.sm.advice.exception;
+
+public class InvalidRefreshTokenException extends RuntimeException {
+
+    public InvalidRefreshTokenException() {
+    }
+
+    public InvalidRefreshTokenException(String message) {
+        super(message);
+    }
+
+    public InvalidRefreshTokenException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
--- src/main/java/com/bowwowcare/sm/service/SignService.java
+++ src/main/java/com/bowwowcare/sm/service/SignService.java
@@ -1,5 +1,6 @@
 package com.bowwowcare.sm.service;
 
+import com.bowwowcare.sm.advice.exception.InvalidRefreshTokenException;
 import com.bowwowcare.sm.advice.exception.LoginFailureException;
 import com.bowwowcare.sm.advice.exception.UserEmailAlreadyExistsException;
 import com.bowwowcare.sm.advice.exception.UserNotFoundException;
