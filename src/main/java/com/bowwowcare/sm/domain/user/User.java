d2fbb8e38c3b963437cd4e1e2708dd0f448fd248
Juuuic <sn990908a@gmail.com>
2023-01-30 17:07:16 +0900

    feat: user 도메인 수정

--- src/main/java/com/bowwowcare/sm/domain/user/User.java
+++ src/main/java/com/bowwowcare/sm/domain/user/User.java
@@ -1,53 +1,59 @@
 package com.bowwowcare.sm.domain.user;
 
 import com.bowwowcare.sm.domain.enums.Role;
+import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
-@Getter
-@NoArgsConstructor
 @Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class User {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
     private Long id;
+
+    @Column(nullable = false, length = 15)
+    private String nickname;
 
     @Column(nullable = false,unique = true)
     private String email;
 
-    @Column(nullable = false)
-    private String name;
+    private String password;
 
-    //@Column(nullable = false, unique = true)
-    //private String nickname;
-
-    @Column
-    private String profileImgUrl;
-
+    @ElementCollection(fetch = FetchType.LAZY)
     @Enumerated(EnumType.STRING)
-    @Column(nullable = false)
-    private Role role;
+    private List<Role> roles = new ArrayList<>();
+
+    private String refreshToken;
+
+    /*@Column
+    private String profileImgUrl;*/
 
     @Builder
-    public User(String name, String email, String profileImgUrl, Role role) {
-        this.name = name;
+    public User(String email, String nickname, String password, List<Role>roles) {
         this.email = email;
-        this.profileImgUrl = profileImgUrl;
-        this.role = role;
+        this.password = password;
+        this.roles = Collections.singletonList(Role.ROLE_USER);
+        this.nickname = nickname;
     }
 
-    public String getRoleKey() {
-        return this.role.getKey();
+    public void addRole(Role role){
+        this.roles.add(role);
     }
 
-    public User update(String name, String profileImgUrl) {
+    /*public User update(String name, String profileImgUrl) {
         this.name = name;
         this.profileImgUrl = profileImgUrl;
 
         return this;
-    }
+    }*/
 }
