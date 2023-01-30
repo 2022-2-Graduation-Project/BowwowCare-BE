7235d9d81a56e5c18fbd864c8dd3228001ce156c
Juuuic <sn990908a@gmail.com>
2023-01-30 17:30:43 +0900

    feat: 회원가입 기능 생성

--- src/main/java/com/bowwowcare/sm/service/SignService.java
+++ src/main/java/com/bowwowcare/sm/service/SignService.java
@@ -1,6 +1,10 @@
 package com.bowwowcare.sm.service;
 
+import com.bowwowcare.sm.domain.user.User;
+import com.bowwowcare.sm.domain.user.UserRepository;
+import com.bowwowcare.sm.jwt.JwtTokenProvider;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -9,5 +13,39 @@ import org.springframework.transaction.annotation.Transactional;
 @RequiredArgsConstructor
 public class SignService {
 
+    private final JwtTokenProvider jwtTokenProvider;
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    /**
+     * dto로 들어온 값을 통해 회원가입 진행
+     * @param userRegisterRequestDto
+     * @return
+     */
+    @Transactional
+    public UserRegisterResponseDto registerMember(UserRegisterRequestDto userRegisterRequestDto) {
+        validateDuplicated(userRegisterRequestDto.getEmail());
+        User user = userRepository.save(
+                User.builder()
+                        .email(userRegisterRequestDto.getEmail())
+                        .password(passwordEncoder.encode(userRegisterRequestDto.getPassword()))
+                        //.provider(null)
+                        .nickname(userRegisterRequestDto.getNickname())
+                        .build());
+
+        return UserRegisterResponseDto.builder()
+                .id(user.getId())
+                .email(user.getEmail())
+                .build();
+    }
+
+    /**
+     * Unique한 값을 가져야함 + 중복된 email을 가질 경우를 검증
+     * @param email
+     */
+    public void validateDuplicated(String email) {
+        if (userRepository.findByEmail(email).isPresent())
+            throw new UserEmailAlreadyExistsException();
+    }
 
 }
