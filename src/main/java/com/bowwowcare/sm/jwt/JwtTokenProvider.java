3b6e6a87a6608e02e1c34c135b2203805aa49d39
Juuuic <sn990908a@gmail.com>
2023-01-30 17:25:11 +0900

    feat: token생성

--- src/main/java/com/bowwowcare/sm/jwt/JwtTokenProvider.java
+++ src/main/java/com/bowwowcare/sm/jwt/JwtTokenProvider.java
@@ -1,10 +1,17 @@
 package com.bowwowcare.sm.jwt;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+import java.util.Base64;
+import java.util.Date;
 
 @Setter
 @Component
@@ -20,4 +27,25 @@ public class JwtTokenProvider {
 
     /** Security UserDetailsService */
     private final UserDetailsService userDetailsService;
+
+
+    // 객체 초기화, secretKey를 Base64로 인코딩
+    @PostConstruct
+    protected void init() {
+        secretKey = Base64.getEncoder().encodeToString(secretKey.getBytes());
+    }
+
+    // JWT 토큰 생성
+    public String createToken(String email) {
+        Claims claims = Jwts.claims().setSubject(email); // JWT payload 에 저장되는 정보단위, 보통 여기서 user를 식별하는 값을 넣는다.
+        Date now = new Date();
+
+        return Jwts.builder()
+                .setClaims(claims) // 정보 저장
+                .setIssuedAt(now) // 토큰 발행 시간 정보
+                .setExpiration(new Date(now.getTime() + tokenValidTime)) // set Expire Time
+                .signWith(SignatureAlgorithm.HS256, secretKey)  // 사용할 암호화 알고리즘과
+                // signature 에 들어갈 secret값 세팅
+                .compact();
+    }
 }
