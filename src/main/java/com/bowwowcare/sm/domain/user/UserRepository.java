27541cb75f48711848af2419c2a16ba93c444037
Juuuic <sn990908a@gmail.com>
2023-01-30 17:10:35 +0900

    style: UserRepository 주석 추가

--- src/main/java/com/bowwowcare/sm/domain/user/UserRepository.java
+++ src/main/java/com/bowwowcare/sm/domain/user/UserRepository.java
@@ -6,5 +6,10 @@ import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
 
+    /**
+     * 이메일로 사용자 계정 단건 조회
+     * @param email 조회할 대상 계정의 이메일
+     * @return Optional 객체 (account 객체 있는 경우), 빈 Optional 객체 (account 가 없는 경우)
+     */
     Optional<User> findByEmail(String email);
 }
