c10a782420658b0a6b6ac0ddf88b248c8dad4ba2
Juuuic <sn990908a@gmail.com>
2023-02-05 23:58:39 +0900

    docs: swagger tag name 변경

--- src/main/java/com/bowwowcare/sm/controller/SignController.java
+++ src/main/java/com/bowwowcare/sm/controller/SignController.java
@@ -19,7 +19,7 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
-@Tag(name = "exception", description = "예외 처리 API")
+@Tag(name = "sign", description = "회원가입/로그인/토큰 API")
 @RestController
 @CrossOrigin
 @RequiredArgsConstructor
