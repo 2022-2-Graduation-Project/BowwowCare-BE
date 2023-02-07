1cebe12a26f5ccdf391e7bad4519b322f250705f
Juuuic <sn990908a@gmail.com>
2023-02-07 17:43:31 +0900

    fix: swagger 토큰 설정 오류 해결

--- src/main/java/com/bowwowcare/sm/config/Swagger2Config.java
+++ src/main/java/com/bowwowcare/sm/config/Swagger2Config.java
@@ -1,7 +1,10 @@
 package com.bowwowcare.sm.config;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springdoc.core.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -17,10 +20,20 @@ public class Swagger2Config {
                 .build();
     }
     @Bean
-    public OpenAPI springShopOpenAPI() {
+    public OpenAPI openAPI() {
+
+        String jwtSchemeName = "X-AUTH-TOKEN";
+        SecurityRequirement securityRequirement = new SecurityRequirement().addList(jwtSchemeName);
+        Components components = new Components().addSecuritySchemes(jwtSchemeName, new SecurityScheme()
+                .name(jwtSchemeName)
+                .in(SecurityScheme.In.HEADER)
+                .type(SecurityScheme.Type.APIKEY));
+
         return new OpenAPI()
                 .info(new Info().title("BowwowCare API")
                         .description("BowwowCare 프로젝트 API 명세서")
-                        .version("v0.0.1"));
+                        .version("v0.0.1"))
+                .addSecurityItem(securityRequirement)
+                .components(components);
     }
 }
