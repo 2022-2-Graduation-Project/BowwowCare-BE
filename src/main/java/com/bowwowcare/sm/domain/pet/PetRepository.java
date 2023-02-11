b30fb82a9242b2b1fb76f669ba6027da234d98e4
Juuuic <sn990908a@gmail.com>
2023-02-12 01:31:19 +0900

    feat: petRepository findByName()추가

--- src/main/java/com/bowwowcare/sm/domain/pet/PetRepository.java
+++ src/main/java/com/bowwowcare/sm/domain/pet/PetRepository.java
@@ -2,5 +2,9 @@ package com.bowwowcare.sm.domain.pet;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface PetRepository extends JpaRepository<Pet, Long> {
+
+    Optional<Pet> findByName(String name);
 }
