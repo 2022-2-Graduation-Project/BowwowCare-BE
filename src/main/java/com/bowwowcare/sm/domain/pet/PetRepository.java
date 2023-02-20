d36ed473c8924ce8b77f29941395ceeb74db9232
Juuuic <sn990908a@gmail.com>
2023-02-20 16:43:40 +0900

    feat: petRepository findPetListByMember 쿼리 추가

--- src/main/java/com/bowwowcare/sm/domain/pet/PetRepository.java
+++ src/main/java/com/bowwowcare/sm/domain/pet/PetRepository.java
@@ -1,10 +1,16 @@
 package com.bowwowcare.sm.domain.pet;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface PetRepository extends JpaRepository<Pet, Long> {
 
     Optional<Pet> findByName(String name);
+
+    @Query(nativeQuery = true, value = "select * from Pet p where p.user_id = :memberId")
+    List<Pet> findPetListByMember(@Param("memberId") Long memberId);
 }
