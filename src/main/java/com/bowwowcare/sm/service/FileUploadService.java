5fb272f46217ba7aefb9f0971aebe99b038b4d13
Juuuic <sn990908a@gmail.com>
2023-02-23 16:20:00 +0900

    feat: file upload servie 추가

---
+++ src/main/java/com/bowwowcare/sm/service/FileUploadService.java
@@ -0,0 +1,57 @@
+package com.bowwowcare.sm.service;
+
+import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.services.s3.model.CannedAccessControlList;
+import com.amazonaws.services.s3.model.ObjectMetadata;
+import com.amazonaws.services.s3.model.PutObjectRequest;
+import com.bowwowcare.sm.domain.file.UploadFile;
+import com.bowwowcare.sm.domain.file.UploadFileRepository;
+import com.bowwowcare.sm.dto.file.FileResponseDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.UUID;
+
+@RequiredArgsConstructor
+@Service
+public class FileUploadService {
+
+    private final AmazonS3Client amazonS3Client;
+    private final UploadFileRepository uploadFileRepository;
+
+    @Value("${cloud.aws.s3.bucket}")
+    private String bucket;
+
+    @Transactional
+    public FileResponseDto saveUploadFile(MultipartFile multipartFile) throws IOException {
+        ObjectMetadata objectMetadata = new ObjectMetadata();
+        objectMetadata.setContentType(multipartFile.getContentType());
+        objectMetadata.setContentLength(multipartFile.getSize());
+
+        String originalFilename = multipartFile.getOriginalFilename();
+        int index = originalFilename.lastIndexOf(".");
+        String ext = originalFilename.substring(index + 1);
+
+        String storeFileName = UUID.randomUUID() + "." + ext;
+        String key = "test/" + storeFileName;
+
+        try (InputStream inputStream = multipartFile.getInputStream()) {
+            amazonS3Client.putObject(new PutObjectRequest(bucket, key, inputStream, objectMetadata)
+                    .withCannedAcl(CannedAccessControlList.PublicRead));
+        }
+
+        String storeFileUrl = amazonS3Client.getUrl(bucket, key).toString();
+        UploadFile uploadFile = new UploadFile(originalFilename, storeFileUrl);
+        uploadFileRepository.save(uploadFile);
+
+        return FileResponseDto.builder()
+                .url(storeFileUrl)
+                .build();
+
+    }
+}
