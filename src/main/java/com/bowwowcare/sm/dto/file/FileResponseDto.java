9bebfcd2ee541948889b18ab38168150f2ffead2
Juuuic <sn990908a@gmail.com>
2023-02-23 16:18:16 +0900

    feat: file response dto 생성

---
+++ src/main/java/com/bowwowcare/sm/dto/file/FileResponseDto.java
@@ -0,0 +1,15 @@
+package com.bowwowcare.sm.dto.file;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class FileResponseDto {
+
+    private String url;
+}
