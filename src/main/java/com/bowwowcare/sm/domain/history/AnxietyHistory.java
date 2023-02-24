fc100330cfed49c762fa6c67605f9473909d205a
Juuuic <sn990908a@gmail.com>
2023-02-25 01:15:47 +0900

    modify: AnxietyHistory column 수정

--- src/main/java/com/bowwowcare/sm/domain/history/AnxietyHistory.java
+++ src/main/java/com/bowwowcare/sm/domain/history/AnxietyHistory.java
@@ -19,14 +19,26 @@ public class AnxietyHistory {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    //공격행동에 대한 건지 분리불안에 대한 건지 판별
-    @Enumerated(EnumType.STRING)
     @Column(nullable = false)
-    private Type type;
+    private boolean situation1;
 
-    //상황 번호(id)
     @Column(nullable = false)
-    private int situation;
+    private boolean situation2;
+
+    @Column(nullable = false)
+    private boolean situation3;
+
+    @Column(nullable = false)
+    private boolean situation4;
+
+    @Column(nullable = false)
+    private boolean situation5;
+
+    @Column(nullable = false)
+    private boolean situation6;
+
+    @Column(nullable = false)
+    private boolean situation7;
 
     @Column
     private LocalDate createdDate;
@@ -36,9 +48,16 @@ public class AnxietyHistory {
     private Pet pet;
 
     @Builder
-    public AnxietyHistory(Type type, int situation, LocalDate createdDate, Pet pet){
-        this.type = type;
-        this.situation = situation;
+    public AnxietyHistory(boolean situation1, boolean situation2, boolean situation3, boolean situation4,
+                                         boolean situation5, boolean situation6, boolean situation7, LocalDate createdDate, Pet pet){
+
+        this.situation1 = situation1;
+        this.situation2 = situation2;
+        this.situation3 = situation3;
+        this.situation4 = situation4;
+        this.situation5 = situation5;
+        this.situation6 = situation6;
+        this.situation7 = situation7;
         this.createdDate = createdDate;
         this.pet = pet;
     }
