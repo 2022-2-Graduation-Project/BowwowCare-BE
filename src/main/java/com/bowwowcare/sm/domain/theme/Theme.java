e31867a867b47e44b8d30a1a8e2ab8c8dad98f6b
Juuuic <sn990908a@gmail.com>
2023-02-28 19:45:54 +0900

    feat: theme entity 생성

---
+++ src/main/java/com/bowwowcare/sm/domain/theme/Theme.java
@@ -0,0 +1,38 @@
+package com.bowwowcare.sm.domain.theme;
+
+import com.bowwowcare.sm.domain.user.Member;
+import lombok.*;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Theme {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private boolean theme1;
+
+    @Column
+    private boolean theme2;
+
+    @Column
+    private boolean theme3;
+
+    @Column
+    private boolean theme4;
+
+    @Column
+    private boolean theme5;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "userId")
+    private Member member;
+}
