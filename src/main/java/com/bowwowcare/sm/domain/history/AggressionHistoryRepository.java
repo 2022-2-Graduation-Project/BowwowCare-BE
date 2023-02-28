5bc3a7f7e2b9d3ba255d88e9c24326cf6cbb5481
Juuuic <sn990908a@gmail.com>
2023-02-28 16:21:48 +0900

    feat: AggressionHistoryRepository query추가

--- src/main/java/com/bowwowcare/sm/domain/history/AggressionHistoryRepository.java
+++ src/main/java/com/bowwowcare/sm/domain/history/AggressionHistoryRepository.java
@@ -1,6 +1,13 @@
 package com.bowwowcare.sm.domain.history;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface AggressionHistoryRepository extends JpaRepository<AggressionHistory,Long> {
+
+    @Query(nativeQuery = true, value = "select * from aggression_history a where a.pet_id = :petId")
+    List<AggressionHistory> findAggressionHistoryListByPet(@Param("petId") Long petId);
 }
