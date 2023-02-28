2bd5be1bd5584954a38f5f58d196ec05b5ade0ad
Juuuic <sn990908a@gmail.com>
2023-02-28 16:18:05 +0900

    feat: AnxietyHistoryRepository query추가

--- src/main/java/com/bowwowcare/sm/domain/history/AnxietyHistoryRepository.java
+++ src/main/java/com/bowwowcare/sm/domain/history/AnxietyHistoryRepository.java
@@ -1,6 +1,13 @@
 package com.bowwowcare.sm.domain.history;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface AnxietyHistoryRepository extends JpaRepository<AnxietyHistory, Long> {
+
+    @Query(nativeQuery = true, value = "select * from anxiety_history a where a.pet_id = :petId")
+    List<AnxietyHistory> findAnxietyHistoryListByPet(@Param("petId") Long petId);
 }
