e87e6d7bcbb42de0f1fb699672d5d115b3148200
Juuuic <sn990908a@gmail.com>
2023-02-28 20:25:53 +0900

    modify: analyzation 관련 모두 삭제

--- src/main/java/com/bowwowcare/sm/domain/analyze/AnalyzationRepository.java
+++
@@ -1,6 +0,0 @@
-package com.bowwowcare.sm.domain.analyze;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AnalyzationRepository extends JpaRepository<Analyzation, Long> {
-}
