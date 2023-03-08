ee9bb1ab05995869a67f7ce60d81b991559306e6
Juuuic <sn990908a@gmail.com>
2023-03-08 15:34:54 +0900

    feat: AggressionTypeRepository 생성

---
+++ src/main/java/com/bowwowcare/sm/domain/care/AggressionTypeRepository.java
@@ -0,0 +1,6 @@
+package com.bowwowcare.sm.domain.care;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AggressionTypeRepository extends JpaRepository<AggressionType, Long> {
+}
